a6ab629633614adad6c01fd84b219c6968f6d5a1
DobriyCoder <DobriyCoder@DESKTOP-9RNCKCN>
2022-05-26 23:12:11 +0300

    Добавьте файлы проекта.

---
+++ Atomex/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Atomex.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
